ab74f34340ec50181207e3842a06695fc8159ec3
Fujii Yosuke <fujiyou1121@gmail.com>
2016-11-05 23:01:37 +0900

    splay heap

--- problem71.scm
+++ problem71.scm
@@ -9,7 +9,19 @@
 (define (heap-single n) '(heap-empty n heap-empty))
 (define (new-heap n h1 h2) '(h1 n h2))
 
-(define 
+(define (partition h piv)
+    (if (heap-null? h)
+        '(heap-empty heap-empty)
+        (if (<= (heap-top h) piv)
+            (let (b (heap-right h))
+                (cond ((heap-null? b) '(h heap-empty))
+                      ((<= (heap-top b) piv)
+                        
+                      )
+                )
+            )
+        )
+    )
 
 (define (heap-insert h n)
  (new-heap (smaller h n) n (bigger h n)))
