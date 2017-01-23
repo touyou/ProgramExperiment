796d89550a74bd06d72643df297d8672f6d55663
Fujii Yosuke <fujiyou1121@gmail.com>
2017-01-23 11:47:24 +0900

    done all problem. congratulation!

--- problem62.scm
+++ problem62.scm
@@ -11,10 +11,10 @@
   (list (empty-frame)))
 (define (extend-env env)
   (cons (empty-frame) env))
-;(define (define-var env var val)
-;  (if (null? env)
-;    env
-;    (cons (update (car env) var val) (cdr env))))
+(define (define-var env var val)
+  (if (null? env)
+    env
+    (cons (update (car env) var val) (cdr env))))
 (define (define-var! env var val)
   (if (null? env)
     #f
@@ -82,7 +82,7 @@
            (res (base-eval env (caddr exp)))
            (env (car res))
            (val (cdr res)))
-      (cons (define-var env var val) var))
+      (cons (define-var! env var val) var))
     (eval-error env 'syntax-error exp)))
 
 (define (var-eval env exp)
@@ -97,12 +97,14 @@
           (val (cdr res)))
       (if val
         (base-eval env (caddr exp))
-        (base-eval env (cdddr exp))))
+        (base-eval env (if (not (null? (cdddr exp)))
+            (cadddr exp)
+            #f))))
     (eval-error env 'syntax-error exp)))
 
 (define (quote-eval env exp)
   (if (correct-syntax? 'quote exp)
-    (cons env (cdr exp))
+    (cons env (cadr exp))
     (eval-error env 'syntax-error exp)))
 
 (define (lambda-eval env exp)
@@ -114,7 +116,7 @@
   (cons env
         (map (lambda (exp) (cdr (base-eval env exp))) el)))
 
-(define (define-vars! env vars vals)
+(define (define-vars env vars vals)
   (if (null? vars)
     env
     (define-vars (define-var env (car vars) (car vals)) (cdr vars) (cdr vals))))
@@ -146,8 +148,8 @@
     (eval-error env 'syntax-error exp)))
 
 (define (base-eval env exp)
-  (print-data exp)
-  (newline)
+  ;(print-data exp)
+  ;(newline)
   (cond ((eof-object? exp) (cons env '*exit*))
         ((constant? exp) (cons env exp))
         ((symbol? exp) (var-eval env exp))
@@ -172,6 +174,10 @@
            (define-var env '+
              (make-primitive 2 (lambda (env args)
                                  (cons env (+ (car args) (cadr args)))))))
+          (env
+            (define-var env '-
+              (make-primitive 2 (lambda (env args)
+                                 (cons env (- (car args) (cadr args)))))))
          (env
            (define-var env '*
              (make-primitive 2 (lambda (env args)
@@ -188,6 +194,14 @@
            (define-var env 'cons
              (make-primitive 2 (lambda (env args)
                                  (cons env (cons (car args) (cadr args)))))))
+         (env
+           (define-var env 'car
+             (make-primitive 1 (lambda (env args)
+                                 (cons env (car (car args)))))))
+         (env
+           (define-var env 'cdr
+             (make-primitive 1 (lambda (env args)
+                                 (cons env (cdr (car args)))))))
          (env
            (define-var env 'list
              (make-primitive #f (lambda (env args) (cons env args)))))
@@ -223,21 +237,21 @@
                      (re-loop env))))))))
   env))
 
-(define (scheme)
-  (let ((top-env (make-top-env)))
-    (define (rep-loop env)
-      (display "sister> ")
-      (let* ((res (base-eval env (read)))
-             (env (car res))
-             (val (cdr res)))
-        (print-data val)
-        ;(let ((info (lookup-var 'former env)))
-        ;  (if (pair? info)
-        ;    (print-data (lookup-var 'latter (closure-env (cdr info))))
-        ;    (display "undefined")))
-        (newline)
-        (if (equal? val '*exit*)
-            #t
-            (rep-loop env))))
-    (let ((top-env (car (base-eval top-env '(load "preamble.scm")))))
-    (rep-loop top-env))))
+;(define (scheme)
+;  (let ((top-env (make-top-env)))
+;    (define (rep-loop env)
+;      (display "sister> ")
+;      (let* ((res (base-eval env (read)))
+;             (env (car res))
+;             (val (cdr res)))
+;        (print-data val)
+;        ;(let ((info (lookup-var 'former env)))
+;        ;  (if (pair? info)
+;        ;    (print-data (lookup-var 'latter (closure-env (cdr info))))
+;        ;    (display "undefined")))
+;        (newline)
+;        (if (equal? val '*exit*)
+;            #t
+;            (rep-loop env))))
+;    (let ((top-env (car (base-eval top-env '(load "preamble.scm")))))
+;    (rep-loop top-env))))
